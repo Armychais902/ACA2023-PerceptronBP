c0cc7a44c882c7da004ffc13ad6515d2e3b17366
Ying-Hua Lee <58675182+Armychais902@users.noreply.github.com>
2023-06-12 13:47:03 +0800

    Initial path perceptron

---
+++ path_perceptron.cc
@@ -0,0 +1,237 @@
+/*
+ * Copyright (c) 2004-2006 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "cpu/pred/path_perceptron.hh"
+
+
+#include <math.h>
+#include "base/intmath.hh"
+#include "base/logging.hh"
+#include "base/trace.hh"
+#include "debug/Fetch.hh"
+
+namespace gem5
+{
+
+namespace branch_prediction
+{
+
+PathPerceptron::PathPerceptron(const PathPerceptronParams &params)
+    : BPredUnit(params),
+      globalHistoryLength(params.globalHistoryLength),
+      localPredictorSize(params.localPredictorSize),
+      globalHistory(params.numThreads, 0),
+      specGlobalHistory(params.numThreads, 0),
+      localWeightBits(16),
+      numLocalWeights(globalHistoryLength + 1),
+      numPerceptrons(localPredictorSize / localWeightBits / numLocalWeights),
+      weights(numPerceptrons, std::vector<int16_t>(numLocalWeights, 0)),
+      indexMask(numPerceptrons - 1)
+{
+    if (!isPowerOf2(localPredictorSize)) {
+        fatal("Invalid local predictor size!\n");
+    }
+
+    if (!isPowerOf2(numPerceptrons)) {
+        fatal("Invalid number of perceptrons! Check globalHistoryLength must be 2^n - 1.\n");
+    }
+
+    globalHistoryMask = (1U << globalHistoryLength) - 1;
+    theta = (int)floor(1.93 * globalHistoryLength + 14);
+
+    // For saturated weight update
+    maxWeight = INT16_MAX;
+    minWeight = INT16_MIN;
+
+    runningSum.assign(globalHistoryLength + 1, 0);
+    specRunningSum.assign(globalHistoryLength + 1, 0);
+
+    branchPath.assign(params.numThreads, std::vector<Addr>());
+
+    DPRINTF(Fetch, "index mask: %#x\n", indexMask);
+
+    DPRINTF(Fetch, "local predictor size: %i\n",
+            localPredictorSize);
+
+    DPRINTF(Fetch, "local weight bits: %i\n", localWeightBits);
+
+    DPRINTF(Fetch, "instruction shift amount: %i\n",
+            instShiftAmt);
+    
+    DPRINTF(Fetch, "global history mask: %#x\n", globalHistoryMask);
+
+    DPRINTF(Fetch, "theta: %i\n", theta);
+}
+
+void PathPerceptron::btbUpdate(ThreadID tid, Addr branch_addr, void * &bp_history)
+{
+    // Place holder for a function that is called to update predictor history when
+    // a BTB entry is invalid or not found.
+    globalHistory[tid] &= (globalHistoryMask & ~1U);
+}
+
+
+void PathPerceptron::updateBranchPath(ThreadID tid, Addr branch_addr)
+{
+    if (branchPath[tid].size() >= globalHistoryLength + 1)
+        branchPath[tid].pop_back();
+    branchPath[tid].insert(branchPath[tid].begin(), branch_addr);
+}
+
+
+int PathPerceptron::updateWeight(int16_t weight, bool taken)
+{
+    // weight hasn't been +- 1
+    if (taken && weight < maxWeight)
+        return weight + 1;
+    if (!taken && weight > minWeight)
+        return weight - 1;
+    return weight;
+}
+
+
+// void PathPerceptron::updateSpecGlobalHistTaken(ThreadID tid)
+// {
+//     specGlobalHistory[tid] = (specGlobalHistory[tid] << 1) | 1;
+//     specGlobalHistory[tid] = specGlobalHistory[tid] & globalHistoryMask;
+// }
+
+// void PathPerceptron::updateSpecGlobalHistNotTaken(ThreadID tid)
+// {
+//     specGlobalHistory[tid] = (specGlobalHistory[tid] << 1);
+//     specGlobalHistory[tid] = specGlobalHistory[tid] & globalHistoryMask;
+// }
+
+
+bool
+PathPerceptron::lookup(ThreadID tid, Addr branch_addr, void * &bp_history)
+{
+    bool taken;
+    unsigned perceptron_idx = getLocalIndex(branch_addr);
+
+    DPRINTF(Fetch, "Looking up index %#x\n",
+            perceptron_idx);
+
+    int y_out = specRunningSum[globalHistoryLength] + weights[perceptron_idx][0];
+    taken = (y_out >= 0) ? true : false;
+
+    DPRINTF(Fetch, "prediction is %i.\n", y_out);
+
+    updateBranchPath(tid, branch_addr);
+
+    int k_j;
+    std::vector<int> specRunningSum_prime(globalHistoryLength + 1, 0);
+    for (int j = 1; j <= globalHistoryLength; j++)
+    {
+        k_j = globalHistoryLength - j;
+        if (taken)
+            specRunningSum_prime[k_j + 1] = specRunningSum[k_j] + weights[perceptron_idx][j];
+        else
+            specRunningSum_prime[k_j + 1] = specRunningSum[k_j] - weights[perceptron_idx][j];
+    }
+    specRunningSum = specRunningSum_prime;
+    specRunningSum[0] = 0;
+
+    specGlobalHistory[tid] = (specGlobalHistory[tid] << 1) | taken;
+    specGlobalHistory[tid] = specGlobalHistory[tid] & globalHistoryMask;
+
+    return taken;
+}
+
+void
+PathPerceptron::update(ThreadID tid, Addr branch_addr, bool taken, void *bp_history,
+                bool squashed, const StaticInstPtr & inst, Addr corrTarget)
+{
+    assert(!bp_history);
+    unsigned perceptron_idx = getLocalIndex(branch_addr);
+
+    // Update non-speculative global history
+    globalHistory[tid] = (globalHistory[tid] << 1) | taken;
+    globalHistory[tid] = globalHistory[tid] & globalHistoryMask;
+
+    // Update non-speculative running sum
+    std::vector<int> runningSum_prime(globalHistoryLength + 1, 0);
+    for (int j = 1; j <= globalHistoryLength; j++)
+    {
+        int k_j = globalHistoryLength - j;
+        if (taken)
+            runningSum_prime[k_j + 1] = runningSum[k_j] + weights[perceptron_idx][j];
+        else
+            runningSum_prime[k_j + 1] = runningSum[k_j] - weights[perceptron_idx][j];
+    }
+    runningSum = runningSum_prime;
+    runningSum[0] = 0;
+
+    unsigned spec_history = specGlobalHistory[tid]; // global history before update to find correlation
+    int y_out = specRunningSum[globalHistoryLength] + weights[perceptron_idx][0];
+    if (squashed || abs(y_out) <= theta) {
+        if (squashed)
+        {
+            specGlobalHistory[tid] = globalHistory[tid];
+            specRunningSum = runningSum;
+        }
+        weights[perceptron_idx][0] = updateWeight(weights[perceptron_idx][0], taken);
+        
+        for (int j = 1; j <= globalHistoryLength; j++)
+        {
+            if (j < branchPath[tid].size())
+            {
+                unsigned k = getLocalIndex(branchPath[tid][j]);    // branchPath[1 ... h]
+                bool correlation = (((spec_history >> j) & 1) == taken);
+                weights[k][j] = updateWeight(weights[k][j], correlation);
+            }
+        }
+    }
+
+    DPRINTF(Fetch, "Looking up index %#x\n", perceptron_idx);
+}
+
+void PathPerceptron::squash(ThreadID tid, void *bp_history)
+{
+    specGlobalHistory[tid] = globalHistory[tid];
+    specRunningSum = runningSum;
+}
+
+// TODO: Can change to consider XOR?
+inline
+unsigned
+PathPerceptron::getLocalIndex(Addr &branch_addr)
+{
+    return (branch_addr >> instShiftAmt) & indexMask;
+}
+
+void
+PathPerceptron::uncondBranch(ThreadID tid, Addr pc, void *&bp_history)
+{
+    updateBranchPath(tid, pc);
+    specGlobalHistory[tid] = (specGlobalHistory[tid] << 1) | 1;
+    specGlobalHistory[tid] = specGlobalHistory[tid] & globalHistoryMask;
+}
+
+} // namespace branch_prediction
+} // namespace gem5
